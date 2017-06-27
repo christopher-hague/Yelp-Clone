3ff8d384cec77f1cf2a74643dc102e446c871f10
christopher-hague <christopher.a.hague@gmail.com>
2017-06-27 15:55:30 -0700

    token

--- yelp_clone_api/app/controllers/api/v1/restaurants_controller.rb
+++ yelp_clone_api/app/controllers/api/v1/restaurants_controller.rb
@@ -37,7 +37,7 @@ class Api::V1::RestaurantsController < ApplicationController
     url = "https://api.yelp.com/v3/businesses/search?location=#{params[:location]}&term=#{params[:term]}"
     begin
       data = RestClient.get(url, headers={
-        'Authorization': 'Bearer 5Zn6pJL5ZHf0xX8d-dJ02A_fvjFvUwY8Ad0nmjVEdpC5tnU8yz5n3VN_uuCH7B_oR-793rH5-B4TIhFkGcYQ_VV3tEZ_YL6Glj1u6Jx-h0M0BZNwGNlob_C-5XY1WXYx'
+        'Authorization': # token goes here
       })
     rescue RestClient::ExceptionWithResponse => e
       # data is being redefined only if there is an error
